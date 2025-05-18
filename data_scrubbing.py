6a29a462f6c93a44c1b3ef76c15c1598b57f62aa
Aihak <95048846+Aihakviper@users.noreply.github.com>
2025-05-18 03:37:28 +0100

    Update data_scrubbing.py

--- data_scrubbing.py
+++ data_scrubbing.py
@@ -1,13 +1,50 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import joblib
+import pandas as pd
 
 ticker = "DXY"
-data = yf.download(ticker, start="2018-01-01", end="2024-12-31")
+data = pd.read_csv("dxy_stock_data.csv")
+
+# dropping unwanted columns
+data.drop(['High','Low','Volume'],axis="columns",inplace=True)
+
+# removing target data 
+target = data['Open']
+new_data = data
+
+new_data.drop(['Open'],axis="columns",inplace=True)
 print(data.head())
 
-data.to_csv("dxy_stock_data.csv")
+# creating x and y arrays
+x = new_data.values
+y = target.values
 
-import matplotlib.pyplot as plt
 
+#plotting graphs
 data["Close"].plot(figsize=(12, 6), title=f"{ticker} Closing Price")
 plt.grid(True)
 plt.show()
+
+target.plot(figsize=(12, 6), title=f"{ticker} Opening Price")
+plt.grid(True)
+plt.show()
+
+#spilitting data
+x_train, x_test , y_train, y_test = train_test_split(x,y,test_size=0.3,random_state=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
