0097d19eba5348a96c2de2556b7d4d12a70d6a08
Aihak <95048846+Aihakviper@users.noreply.github.com>
2025-05-28 11:20:36 +0100

    Update data_scrubbing.py

--- data_scrubbing.py
+++ data_scrubbing.py
@@ -1,50 +1,18 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-import joblib
-import pandas as pd
 
-ticker = "DXY"
-data = pd.read_csv("dxy_stock_data.csv")
+import pandas as pd
+ 
+
+data = pd.read_csv("AAPL.csv")
 
 # dropping unwanted columns
-data.drop(['High','Low','Volume'],axis="columns",inplace=True)
+data.drop(['Close'],axis="columns",inplace=True)
 
 # removing target data 
-target = data['Open']
-new_data = data
-
-new_data.drop(['Open'],axis="columns",inplace=True)
-print(data.head())
+target = data['Open'].copy()
+new_data = data.copy()
+new_data.drop(['Open','Date'],axis="columns", inplace=True)
 
 # creating x and y arrays
-x = new_data.values
-y = target.values
-
-
-#plotting graphs
-data["Close"].plot(figsize=(12, 6), title=f"{ticker} Closing Price")
-plt.grid(True)
-plt.show()
-
-target.plot(figsize=(12, 6), title=f"{ticker} Opening Price")
-plt.grid(True)
-plt.show()
-
-#spilitting data
-x_train, x_test , y_train, y_test = train_test_split(x,y,test_size=0.3,random_state=0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+x = new_data[:-1]
+y = target.shift(-1)
+y.dropna(inplace=True)
